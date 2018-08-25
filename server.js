7fedd31b3220999d7e6db8fbbe85188e84402b38
Maheen Heera Syed <maheenheerasyed@gic.com.sg>
2018-08-26 03:15:43 +0800

    Changes to packapge.json for heroku

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-store'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname+'/dist/angular-store/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-store/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
